edc12218ffe96e9636e1e3f7cc53a4b70493e85b
Jameka Echols <jameka.echols@gmail.com>
2018-08-15 14:37:35 -0500

    Adding pictures

--- ImageViewer/ViewController.swift
+++ ImageViewer/ViewController.swift
@@ -6,13 +6,28 @@
 //  Copyright © 2018 Jameka Echols. All rights reserved.
 //
 
-import UIKit
+import UIKit  // file to reference the iOS user interface kit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    var pictures = [String]() //array of file names (pictures), () =create now!
+    
+    
+    override func viewDidLoad() { // override means to change Apple's default behavior
+        super.viewDidLoad() // super tells Apple to run UIViewController before mine
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let fm = FileManager.default //file manager constant to use the filesystem
+        let path = Bundle.main.resourcePath! // show me where I can find my images I added to the file
+        let items = try! fm.contentsOfDirectory(atPath: path) // an array of the file names that were found in the
+        
+        for item in items {
+            if item.hasPrefix("IMG") {
+                // the picture should load
+                pictures.append(item) //load the picture file names to the array
+            }
+        }
+        print(pictures)
     }
 
     override func didReceiveMemoryWarning() {
